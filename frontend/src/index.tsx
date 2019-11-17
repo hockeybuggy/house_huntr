e65b495df766000c14dbf0593cae0ff6fb6e2e21
Douglas Anderson <hockeybuggy@gmail.com>
2019-11-16 20:39:56 -0500

    Setup hot reloading.
    This involved changing how webpack was used completely. Instead it uses
    https://github.com/gaearon/react-hot-loader/tree/298a7fccefa9d9e61be83006426eeeaed3afe712/examples/typescript
    which involved switching to using babel.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,14 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { App } from "./components/App";
 
+const root = document.createElement('div')
+document.body.appendChild(root)
+
 ReactDOM.render(
     <App compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    root
 );
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,40 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 module.exports = {
-    mode: "production",
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx"]
+  mode: 'development',
+  entry: ['./src/index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    modules: ['node_modules'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
     },
-
-    module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "ts-loader"
-                    }
-                ]
-            },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader"
-            }
-        ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    }
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)s(x)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            babelrc: false,
+            presets: [
+              [
+                '@babel/preset-env',
+                { targets: { browsers: 'last 2 versions' } }, // or whatever your project requires
+              ],
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+            ],
+            plugins: [
+              ['@babel/plugin-proposal-class-properties', { loose: true }],
+              'react-hot-loader/babel',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+  plugins: [new ForkTsCheckerWebpackPlugin(), new webpack.NamedModulesPlugin(), new HtmlWebpackPlugin()],
 };
