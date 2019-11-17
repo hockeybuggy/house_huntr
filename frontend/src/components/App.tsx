0a560bde660977fbe86ce53290db0dfa59b387d6
Douglas Anderson <hockeybuggy@gmail.com>
2019-11-16 20:47:42 -0500

    Install prettier.
    I borrowed a config I had laying around for another project (a wedding
    website for a friend). I also setup some more scripts for running the
    app.

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
 
-export interface AppProps { compiler: string; framework: string; }
+export interface AppProps {
+  compiler: string;
+  framework: string;
+}
 
-export const App = (props: AppProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export const App = (props: AppProps) => (
+  <h1>
+    Hello from {props.compiler} and {props.framework}!
+  </h1>
+);
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,11 @@
-import 'core-js/stable'
-import 'regenerator-runtime/runtime'
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { App } from "./components/App";
 
-const root = document.createElement('div')
-document.body.appendChild(root)
+const root = document.createElement("div");
+document.body.appendChild(root);
 
-ReactDOM.render(
-    <App compiler="TypeScript" framework="React" />,
-    root
-);
+ReactDOM.render(<App compiler="TypeScript" framework="React" />, root);
